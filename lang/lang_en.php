bd0623a9ab956eea7a2ffda9e465178389466031
MALOANI SAIDI GEORGES <157014974+Maloani@users.noreply.github.com>
2026-03-04 14:34:23 +0100

    Add files via upload

---
+++ lang/lang_en.php
@@ -0,0 +1,15 @@
+<?php
+$lang = [
+    'title' => "University of Lisala - Home",
+    'welcome' => "Biometric Management System",
+    'description' => "Welcome to the secure platform of the University of Lisala. This system allows administrators to manage access, students, and academic data using biometrics.",
+    'login_button' => "Login",
+    'login_title' => "Admin Login",
+    'username' => "Username",
+    'password' => "Password",
+    'submit' => "Login",
+    'lang_fr' => "French",
+    'lang_en' => "English",
+    'error_required' => "Please fill in all fields.",
+    'error_invalid' => "Invalid credentials."
+];
---
+++ lang/lang_fr.php
@@ -0,0 +1,17 @@
+<?php
+// Fichier de langue : Français (fr)
+$lang = [
+    'title' => "Université de Lisala - Accueil",
+    'welcome' => "Système de Gestion Biométrique",
+    'description' => "Bienvenue sur la plateforme sécurisée de l'Université de Lisala. Ce système permet aux administrateurs de gérer les accès, les étudiants et les données académiques à l’aide de la biométrie.",
+    'login_button' => "Se connecter",
+    'login_title' => "Connexion Administrateur",
+    'username' => "Nom d'utilisateur",
+    'password' => "Mot de passe",
+    'submit' => "Connexion",
+    'lang_fr' => "Français",
+    'lang_en' => "Anglais",
+    'error_required' => "Veuillez remplir tous les champs.",
+    'error_invalid' => "Identifiants incorrects."
+];
+?>
